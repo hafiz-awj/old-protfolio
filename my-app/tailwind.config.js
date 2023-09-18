a5f6bc940e8f59817d1ae0046db242f62ddc652f
hafiz-awj <hafizaliawj@gmail.com>
2023-09-18 20:50:45 +0500

    tailwind

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
